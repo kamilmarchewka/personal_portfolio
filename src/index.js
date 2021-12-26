9452bab38d3b4566ce7ff4a00679340f504af819
Kamil <kamilm2003@gmail.com>
2021-12-26 16:21:16 +0100

    basic setup, landing page header + nav for mobile

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./main.scss";
+const navContent = document.querySelector(".navigation__content");
+
+document.querySelector(".toggler").addEventListener("click", function () {
+      if (this.getAttribute("aria-expanded") === "false") {
+            this.setAttribute("aria-expanded", "true");
+            navContent.classList.add("nav-opening");
+      } else {
+            this.setAttribute("aria-expanded", "false");
+            navContent.classList.add("nav-closing");
+            setTimeout(() => {
+                  navContent.classList.remove("nav-opening");
+                  navContent.classList.remove("nav-closing");
+            }, 260);
+      }
+});
