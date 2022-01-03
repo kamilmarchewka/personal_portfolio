c326df3cee0dcaa5957fa2ebea7aa68ea9e71250
Kamil <kamilm2003@gmail.com>
2022-01-03 11:00:06 +0100

    asdf

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const HtmlWebpackTagsPlugin = require("html-webpack-tags-plugin");
+const SocialTags = require("social-tags-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -24,6 +25,29 @@ module.exports = merge(common, {
     //       },
     //     },
     //   ],
+    // }),
+    // new SocialTags({
+    //   appUrl: "http://kamilmarchewka.design/",
+    //   facebook: {
+    //     // 'fb:app_id': "123456789",
+    //     "og:url": "http://kamilmarchewka.design/index.html",
+    //     "og:type": "website",
+    //     "og:title": "Content Title",
+    //     "og:image": "src/og_home.png",
+    //     "og:description": "Description Here",
+    //     "og:site_name": "Site Name",
+    //     // 'og:locale': "en_US",
+    //     // 'og:article:author': "",
+    //   },
+    //   twitter: {
+    //     // "twitter:card": "summary",
+    //     // "twitter:site": "@site_account",
+    //     // "twitter:creator": "@individual_account",
+    //     "twitter:url": "http://kamilmarchewka.design/index.html",
+    //     "twitter:title": "Content Title",
+    //     "twitter:description": "Content description less than 200 characters",
+    //     "twitter:image": "./src/og_home.png",
+    //   },
     // }),
   ],
   module: {
