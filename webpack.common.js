0ee9a92da80cf651de2fc69ef771da39c2302717
Kamil <kamilm2003@gmail.com>
2022-01-03 17:55:17 +0100

    adding all og tags

--- webpack.common.js
+++ webpack.common.js
@@ -6,30 +6,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/template.html",
       favicon: "./src/favicon.png",
-      // meta: {
-      // 'Content-Security-Policy': { 'http-equiv': 'Content-Security-Policy', 'content': 'default-src https:' },
-      // // Will generate: <meta http-equiv="Content-Security-Policy" content="default-src https:">
-      // // Which equals to the following http header: `Content-Security-Policy: default-src https:`
-      // 'set-cookie': { 'http-equiv': 'set-cookie', content: 'name=value; expires=date; path=url' },
-      // // Will generate: <meta http-equiv="set-cookie" content="value; expires=date; path=url">
-      // // Which equals to the following http header: `set-cookie: value; expires=date; path=url`
-      // "og:image": { content: "./src/og_home.png" },
-      // <meta property="og:image" content="./og_home.png" />
-
-      // },
-      // meta: {
-      // 'description': { name: 'description', contnet: '...' },
-      // 'keyword': { name: 'keywords', content: '...' },
-      // 'og:title': { property: 'og:title', content: '...' },
-      // 'og:description': { property: 'og:description', content: '...' },
-      // 'og:type': { property: 'og:type', content: 'website' },
-      // 'og:url': { property: 'og:url', content: '...' },
-      // "og:image": { property: "og:image", content: "/og_home.png" },
-      // 'twitter:card': { name: 'twitter:card', content: 'summary_large_image' },
-      // 'twitter:title': { name: 'twitter:title', content: '...' },
-      // 'twitter:description': { name: 'twitter:description', content: '...' },
-      // 'twitter:image': { name: 'twitter:image', content: '...' }
-      // },
     }),
   ],
   module: {
@@ -38,31 +14,6 @@ module.exports = {
         test: /\.html$/,
         use: ["html-loader"],
       },
-      // {
-      //   test: /\.html$/i,
-      //   loader: "html-loader",
-      //   options: {
-      //     sources: {
-      //       list: [
-      //         {
-      //           tag: "meta",
-      //           attribute: "content",
-      //           type: "src",
-      //           filter: (tag, attribute, attributes, resourcePath) => {
-      //             if (
-      //               attributes.value === "og:image" ||
-      //               attributes.name === "twitter:image"
-      //             ) {
-      //               return true;
-      //             }
-
-      //             return false;
-      //           },
-      //         },
-      //       ],
-      //     },
-      //   },
-      // },
     ],
   },
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -6,6 +6,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const HtmlWebpackTagsPlugin = require("html-webpack-tags-plugin");
 const SocialTags = require("social-tags-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -16,39 +17,14 @@ module.exports = merge(common, {
   plugins: [
     new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }),
     new CleanWebpackPlugin(),
-    // new HtmlWebpackTagsPlugin({
-    //   metas: [
-    //     {
-    //       path: "og_home.png",
-    //       attributes: {
-    //         property: "og:image",
-    //       },
-    //     },
-    //   ],
-    // }),
-    // new SocialTags({
-    //   appUrl: "http://kamilmarchewka.design/",
-    //   facebook: {
-    //     // 'fb:app_id': "123456789",
-    //     "og:url": "http://kamilmarchewka.design/index.html",
-    //     "og:type": "website",
-    //     "og:title": "Content Title",
-    //     "og:image": "src/og_home.png",
-    //     "og:description": "Description Here",
-    //     "og:site_name": "Site Name",
-    //     // 'og:locale': "en_US",
-    //     // 'og:article:author': "",
-    //   },
-    //   twitter: {
-    //     // "twitter:card": "summary",
-    //     // "twitter:site": "@site_account",
-    //     // "twitter:creator": "@individual_account",
-    //     "twitter:url": "http://kamilmarchewka.design/index.html",
-    //     "twitter:title": "Content Title",
-    //     "twitter:description": "Content description less than 200 characters",
-    //     "twitter:image": "./src/og_home.png",
-    //   },
-    // }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "src/og_home.png",
+          to: "og_home.png",
+        },
+      ],
+    }),
   ],
   module: {
     rules: [
